61853abdbb9e4a6a6742f8c10ba152444e8d3cf7
itsmebgj <bgj3088@gmail.com>
2025-01-31 21:51:23 +0900

    ~43

--- src/lv0/Solution41.java
+++ src/lv0/Solution41.java
@@ -1,21 +1,21 @@
 package lv0;
 
+import java.util.Arrays;
 
 class Solution41 {
     public int[] solution(int[] num_list) {
-        int[] answer = {};
-        int[] tmp1_list = 
-
         int length = num_list.length;
+        int[] tmp1_list = Arrays.copyOf(num_list, length+1);
+        int[] tmp2_list = Arrays.copyOf(num_list, length+1);
 
         for(int i =0; i < length; i++){
             if(num_list[length-1] > num_list[length-2]){
-                num_list[i] += num_list[length-1] - num_list[length-2];
+                tmp1_list[length] = num_list[length-1] - num_list[length-2];
+                return tmp1_list;
             }else{
-
+                tmp2_list[length] = num_list[length-1] * 2;
             }
         }
-
-        return answer;
+        return tmp2_list;
     }
 }
